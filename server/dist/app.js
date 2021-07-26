f54119e71946e252f504a795524f2ecadc10bd0f
biancasarabia <biols@cin.ufpe.br>
2021-07-26 10:25:18 -0300

    adicionando servidor base

---
+++ server/dist/app.js
@@ -0,0 +1,19 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = express_1.default();
+const port = 3000;
+console.log(port);
+app.get('/', (req, res) => {
+    res.send('The sedulous hyena ate the antelope!');
+});
+app.listen(port, () => {
+    //if (err) {
+    //  return console.error(err);
+    //}
+    return console.log(`server is listening on ${port}`);
+});
+//# sourceMappingURL=app.js.map
