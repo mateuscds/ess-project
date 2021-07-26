189a25e75dddf0b51db39eefc55fa745da34fd11
biancasarabia <biols@cin.ufpe.br>
2021-07-26 11:02:01 -0300

    criando classes de usuario, professor e aluno - em progresso

---
+++ common/alunos.ts
@@ -0,0 +1,29 @@
+import { Usuario } from './usuario';
+
+function calculaMascara(cpf: string) { 
+    var parte1 = cpf.substring(0, 3);
+    var parte2 = cpf.substring(3, 6);
+    var parte3 = cpf.substring(6, 9);
+    var parte4 = cpf.substring(9, 11);
+
+    var parte1_numero = 4*Number(parte1);
+    var parte2_numero = 3*Number(parte2);
+    var parte3_numero = 2*Number(parte3);
+    var parte4_numero = Number(parte4);
+
+    var mascara = "" + parte1_numero + '_' + parte2_numero + '_' + parte3_numero + '_' + parte4_numero
+    
+    return mascara;
+ }
+
+export class Aluno extends Usuario{
+    private mascara: string;
+
+    get Mascara(): string{
+		return this.mascara;
+	}
+
+	set Mascara(){
+        this.mascara = calculaMascara(this.Cpf);
+	}
+}
